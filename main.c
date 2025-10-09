a1b8f3da3b19dc3e7666d71dad6a8a98bcc60983
PierreSoreau <soreaupierre@gmail.com>
2025-10-09 10:23:09 +0000

    add : sur fin de phrase choisissez un nombre...;

--- main.c
+++ main.c
@@ -8,31 +8,31 @@ int main(){
     int random_number = rand() % 100 + 1;
 
     int nb1;
-    printf("Choisissez un nombre entier entre 1 et 100\n");
+    printf("Choisissez un nombre entier entre 1 et 100:\n");
     scanf("%d",&nb1);
 
     while(nb1!=random_number){
 
         if ( (nb1>100) || (nb1<1) ){
 
-            printf("Le nombre entier à choisir doit être entre 1 et 100\n");
-            printf("Choisissez un nombre entier entre 1 et 100\n");
+            printf("Le nombre entier à choisir doit être entre 1 et 100.\n");
+            printf("Choisissez un nombre entier entre 1 et 100:\n");
             scanf("%d",&nb1);
         }
 
         else if (nb1<random_number){
             printf("C'est plus!\n");
-            printf("Choisissez un nombre entier entre 1 et 100\n");
+            printf("Choisissez un nombre entier entre 1 et 100:\n");
             scanf("%d",&nb1);
         }
 
         else{
             printf("C'est moins!\n");
-            printf("Choisissez un nombre entier entre 1 et 100\n");
+            printf("Choisissez un nombre entier entre 1 et 100:\n");
             scanf("%d",&nb1);
 
         }
     }
 
-    printf("C'est gagné!\n");
+    printf("Gagné!\n");
 }
