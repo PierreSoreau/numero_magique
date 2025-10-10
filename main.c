c250bb31c1017d25f4e928e54cf846ce1b8dcc0e
PierreSoreau <soreaupierre@gmail.com>
2025-10-10 20:53:40 +0000

    rajout de l'erreur dans le cas d'un nombre décimal

--- main.c
+++ main.c
@@ -8,19 +8,22 @@ int main(){
     int random_number = rand() % 100 + 1;
 
     /*l'utilisateur essaie de deviner pour la première fois le nombre entier mystère de l'ordinateur */
-    int nb1;    
+    int nb1; 
+    char c;   
     printf("Choisissez un nombre entier entre 1 et 100:\n");    
-    int ret =scanf("%d", &nb1);    
+    int ret =scanf("%d%c", &nb1,&c); //on lit le nombre entier et le caractère suivant   
 
         /*Tant que l'élément choisi par l'utilisateur ne correspond pas au nombre entier choisi par l'ordinateur on rentre dans les conditions de la boucle while.*/
         while(nb1!=random_number){  
             
-            //Si la valeur rentrée par l'utilisateur n'est pas un entier 
-            if (ret==0){
+            //Si la valeur rentrée par l'utilisateur n'est pas un entier (caractère ou nombre décimal) on rentre dans le if
+            //dans le cas d'un nombre décimal par exemple l'ordinateur arrive à lire 2 valeurs pour le scan donc ret=2. Mais si c'est 2.5 par exemple il lit 2 et '.' mais c correspond à '.' et non pas \n
+            //dans le cas d'un ou plusieurs caractères l'ordinateur ne lit aucune valeur ou une seule pour le scan. Si c'est blabla par exemple il lit le 'l' seulement donc ret=1 et en plus c='l'
+            if (ret!=2 || c!='\n'){
                 printf("Vous n'avez pas rentré un nombre entier\n");
-                while (getchar()!= '\n');               
+                while (getchar()!= '\n');//on réinitialise le buffer jusqu'à la fin de la ligne               
                 printf("Choisissez un nombre entier entre 1 et 100:\n");                
-                ret=scanf("%d", &nb1);                       
+                ret =scanf("%d%c", &nb1,&c);//on relit de nouveau une nouvelle valeur rentrée par l'utilisateur                        
                 
             }
 
@@ -31,7 +34,7 @@ int main(){
 
                 printf("Le nombre entier à choisir doit être entre 1 et 100.\n");
                 printf("Choisissez un nombre entier entre 1 et 100:\n");
-                ret=scanf("%d",&nb1);
+                ret =scanf("%d%c", &nb1,&c);
             }
 
 
@@ -41,7 +44,7 @@ int main(){
             else if (nb1<random_number){
                 printf("C'est plus!\n");
                 printf("Choisissez un nombre entier entre 1 et 100:\n");
-                ret=scanf("%d",&nb1);
+                ret =scanf("%d%c", &nb1,&c);
             }
 
             /*Dernière possibilité, si le nombre entier choisi par l'utilisateur est supérieur au nombre de l'ordinateur 
@@ -50,7 +53,7 @@ int main(){
             else{
                 printf("C'est moins!\n");
                 printf("Choisissez un nombre entier entre 1 et 100:\n");
-                ret=scanf("%d",&nb1);
+                ret =scanf("%d%c", &nb1,&c);
 
             }
         }
